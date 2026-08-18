a96640b7b0eac2f024f3dd4d25bfd3b19aa8ed42
seja2b <seja2b.br@gmail.com>
2026-08-18 18:01:46 -0300

    security: restrict settings updates to admins

--- src/routes/settingsRoutes.js
+++ src/routes/settingsRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
+import { requireRole } from '../middleware/roleMiddleware.js'
 import { getSettings, updateSettings } from '../controllers/settingsController.js'
 
 const router = express.Router()
 
 router.get('/', getSettings)
-router.put('/', authMiddleware, updateSettings)
+router.put('/', authMiddleware, requireRole('ADMIN'), updateSettings)
 
 export default router
